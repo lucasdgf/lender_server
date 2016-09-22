8e1f0d97ece1b3455bd71d35637cbf4b062ae5f9
Lucas Freitas <lucasfreitas@Lucass-MacBook-Air.local>
2016-09-22 10:19:36 -0700

    Uploading work to git

---
+++ public/javascripts/main.js
@@ -0,0 +1,19 @@
+// Create IE + others compatible event handler (from https://davidwalsh.name/window-iframe)
+var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+var eventer = window[eventMethod];
+var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+// Listen to message from child window
+eventer(messageEvent,function(e) {
+  var label = $('#application-status');
+  var message = e.data ? 'Approved' : 'Denied';
+
+  label.removeClass('label-warning');
+  label.html(message);
+  // Success
+  if (e.data) {
+    label.addClass('label-success');
+  } else {
+    label.addClass('label-danger');
+  }
+},false);
